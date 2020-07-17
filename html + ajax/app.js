2f6f044d51055481e3c7d25ae7b7356361a107d3
patrickayella04 <60228005+patrickayella04@users.noreply.github.com>
2020-07-17 15:49:56 +0100

    navi drop and hamburger menue

--- html + ajax/app.js
+++ html + ajax/app.js
@@ -1,14 +1,19 @@
 const xhr = new XMLHttpRequest();
 const container = document.getElementById('container');
+// const head = document.getElementById('head');
 
 xhr.onload = function () {
     if (xhr.status === 200) {
         container.innerHTML = xhr.responseText;
+        // head.innerHTML = xhr.responseText
     } else {
         console.warn('Did not recieve 200 of from response!')
     }
-
 };
 
+// xhr.open('GET', 'header.html', true);
 xhr.open('GET', 'colors.html', true);
-xhr.send(null);
+xhr.send(null); 
+
+
+    
