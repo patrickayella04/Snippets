de6573aa367196275462d66814756063cf6c2e4a
patrickayella04 <60228005+patrickayella04@users.noreply.github.com>
2020-07-22 23:26:22 +0100

    js start, customer json string, JSON.parse() string to javaScript object to access index value, and inject into innerHTML

--- ajax + jason/app.js
+++ ajax + jason/app.js
@@ -1,3 +1,33 @@
+document.getElementById('button1').addEventListener('click', loadCustomer);
+
+function loadCustomer(e) {
+    const xhr = new XMLHttpRequest();
+
+    xhr.open('GET', 'customer.json', true);
+
+    xhr.onload = function () {
+        if (this.status === 200) {
+            // console.log(this.responseText)
+
+            const customer = JSON.parse(this.responseText); // turns json string into javaScript object...thus we can access using customer.name, or customer.id and put the index's values into our page.
+
+            const output =
+                `<ul>
+                    <li>ID: ${customer.id}</li>
+                    <li>Name: ${customer.name}</li>
+                    <li>Company: ${customer.company}</li> 
+                    <li>Phone: ${customer.phone}</li>
+                </ul>`;
+            
+            document.getElementById('customer').innerHTML = output;
+
+
+        }
+    }
+
+    xhr.send();
+}
+
 
 
 
